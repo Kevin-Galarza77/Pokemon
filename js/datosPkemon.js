bc380736c1b00522bde8d58759802b8e61daf464
Kevin-Galarza10 <jimenezkev1040@gmail.com>
2022-11-24 23:24:33 -0500

    Finished

--- js/datosPkemon.js
+++ js/datosPkemon.js
@@ -19,75 +19,75 @@ const btnSeleccionar = () => {
 
 btnSeleccionar();
 
-const lista = document.getElementById("listarpokemon");
-const modals = document.getElementById("modals");
-const d=document;
+const d = document;
+const lista = d.getElementById("listarpokemon");
+
+
 
 const pintarPokemon = (data, id) => {
-    let item = lista.querySelector(`#pok-${id}`);
-    let modal = modals.querySelector(`#modal-pok-${id}`);
-    item.getElementsByTagName("img")[0].setAttribute("src", data.sprites.front_default);
-    item.getElementsByTagName("p")[0].innerHTML = data.name.toUpperCase();
-    modal.getElementsByTagName("h2")[0].innerHTML = data.name.toUpperCase();
-    let $imagenes = '';
-    let $contenedor=d.createElement("div");
+
+    let $item = lista.querySelector(`#pok-${id}`);
+    $item.getElementsByTagName("img")[0].setAttribute("src", data.sprites.front_default);
+    $item.getElementsByTagName("p")[0].innerHTML = data.name.toUpperCase();
+
+    let skills = ``
+    for (let index = 0; index < data.abilities.length; index++) {
+        skills += `<li>${data.abilities[index].ability.name} </li>`
+    }
+    $item.getElementsByTagName("ol")[0].innerHTML = skills;
+    pintarModalPokemon(data, id);
+}
+
+const modals = d.getElementById("modals");
+
+const pintarModalPokemon = (data, id) => {
+
+    const $modal = modals.querySelector(`#modal-pok-${id}`);
+    const $modalBody = $modal.getElementsByClassName("modal-body")[0];
+    $modalBody.innerHTML='';
+    $modal.getElementsByTagName("h2")[0].innerHTML = data.name.toUpperCase();
+
+    const $contenedorImagenes = d.createElement("div");
+
     for (const property in data.sprites) {
         if (data.sprites[property] && typeof (data.sprites[property]) === "string") {
-            let $imagen = new Image();
-            $imagen.src=data.sprites[property];
-            $contenedor.appendChild($imagen);
+            const $imagen = new Image();
+            $imagen.src = data.sprites[property];
+            $contenedorImagenes.appendChild($imagen);
         }
     }
-    modal.getElementsByClassName("modal-body")[0].appendChild($contenedor);
+
+    addElemento($modalBody,$contenedorImagenes);
+
+    const detalles = ["Experiencia Base", data.base_experience, "Altura (Decímetros)", data.height, "Peso (Hectogramos)", data.weight, "Especie", data.species.name];
+
+    for (let index = 0; index < detalles.length; index += 2) {
+        const $contenedorDescripciones = d.createElement("div");
+        $contenedorDescripciones.className = "d-flex justify-content-between align-items-center";
+        const $descripcion = `<h6 class="text-primary fs-6">${detalles[index]}</h6> <p class="text-dark fs-6">${detalles[index + 1]}</p>`;
+        $contenedorDescripciones.innerHTML = $descripcion;
+
+        addElemento($modalBody,$contenedorDescripciones);
     
-    let detalles = ["Experiencia Base",data.base_experience,"Altura (Decímetros)",data.height,"Peso (Hectogramos)",data.weight,"Especie",data.species.name]
-    
-    
-    for (let index = 0; index < detalles.length; index+=2) {
-        let $contenedorDescripciones=d.createElement("div")
-        $contenedorDescripciones.className="d-flex justify-content-between align-items-center";
-        let $descripcion=`<h6 class="text-primary fs-6">${detalles[index]}</h6> <p class="text-dark fs-6">${detalles[index+1]}</p>`;
-        $contenedorDescripciones.innerHTML=$descripcion;
-        console.log($contenedorDescripciones);
-        modal.getElementsByClassName("modal-body")[0].appendChild($contenedorDescripciones);
-        modal.getElementsByClassName("modal-body")[0].appendChild(d.createElement("hr"));
     }
 
-    // $imagenes += `<hr>
-    // <div class="d-flex justify-content-between align-items-center">
-    //     <h6 class="text-primary fs-6">Experiencia Base</h6>
-    //     <p class="text-dark fs-6">${data.base_experience}</p>
-    // </div>
-    // <hr>
-    // <div class="d-flex justify-content-between align-items-center">
-    //     <h6 class="text-primary fs-6">Altura (Decímetros)</h6>
-    //     <p class="text-dark fs-6">${}</p>
-    // </div>
-    // <hr>
-    // <div class="d-flex justify-content-between align-items-center">
-    //     <h6 class="text-primary fs-6"></h6>
-    //     <p class="text-dark fs-6">${}</p>
-    // </div>
-    // <hr>
-    // <div class="d-flex justify-content-between align-items-center">
-    //     <h6 class="text-primary fs-6"></h6>
-    //     <p class="text-dark fs-6">${}</p>
-    // </div>
-    // <hr>
-    // <div class="d-flex justify-content-between align-items-center">
-    //     <h6 class="text-primary fs-6">Slot y Tipos</h6>
-    // `
+    const $divIndices = d.createElement("div");
+    $divIndices.className = "d-flex justify-content-between align-items-start";
 
-    let game_indices = "<ul class='list-group list-group-numbered'>";
+    let game_indices = "<h6 class='text-primary fs-6'>Slot y Tipos</h6><ol class='list-group list-group-numbered'>";
     for (let index = 0; index < data.types.length; index++) {
         game_indices += `<li class='text-end'>${data.types[index].slot} ${data.types[index].type.name}</li>`
     }
-    game_indices += "</ul>"
-    $imagenes += game_indices + `</div><hr>
-        <div class="d-flex justify-content-between align-items-center">
-        <h6 class="text-primary fs-6">Lista de Elementos</h6>
-    `
-    let elementos = "<ul class='list-group list-group-numbered'>";
+    game_indices += "</ol>";
+    $divIndices.innerHTML = game_indices;
+
+    addElemento($modalBody,$divIndices);
+
+    const divElementos = d.createElement("div");
+    divElementos.className = "d-flex justify-content-between align-items-start";
+    
+    let elementos = "<h6 class='text-primary fs-6'>Lista de Elementos</h6><ul class='list-group list-group-numbered'>";
+
     if (!data.held_items.length) {
         elementos += "Ninguno";
     } else {
@@ -96,11 +96,13 @@ const pintarPokemon = (data, id) => {
         }
     }
     elementos += "</ul>"
-    $imagenes += elementos + `</div><hr>`
-    // modal.getElementsByClassName("modal-body")[0].innerHTML = $imagenes;
-    let skills = ``
-    for (let index = 0; index < data.abilities.length; index++) {
-        skills += `<li>${data.abilities[index].ability.name} </li>`
-    }
-    item.getElementsByTagName("ol")[0].innerHTML = skills;
+    divElementos.innerHTML = elementos;
+
+    addElemento($modalBody,divElementos);
+
+}
+
+const addElemento=($modal,$elemento)=>{
+    $modal.appendChild($elemento);
+    $modal.appendChild(d.createElement("hr"));
 }
